834f861fcb8074b71c59551d6b005719f61abcef
Ryoma Fujiwara <e238058r@hotmail.com>
2022-08-09 01:47:13 +0900

    タスク定義名およびコンテナ定義を出力するよう設定

--- modules/ecs/task/outputs.tf
+++ modules/ecs/task/outputs.tf
@@ -2,3 +2,13 @@ output "task_definition_arn" {
   description = "タスク定義のARN"
   value       = aws_ecs_task_definition.task_definition.arn
 }
+
+output "task_family_name" {
+  description = "タスク定義の名前"
+  value = aws_ecs_task_definition.task_definition.family
+}
+
+output "container_definitions" {
+  description = "タスク定義に含まれるコンテナ定義"
+  value = jsondecode(aws_ecs_task_definition.task_definition.container_definitions)
+}
