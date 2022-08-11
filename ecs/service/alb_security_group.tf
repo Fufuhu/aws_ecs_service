1c451aa62e1bcfa4d80344f41bb230830f89f085
Ryoma Fujiwara <e238058r@hotmail.com>
2022-08-11 23:21:45 +0900

    ALB用のセキュリティグループを作成

---
+++ ecs/service/alb_security_group.tf
@@ -0,0 +1,10 @@
+
+module "alb_security_group" {
+  source = "../../modules/vpc/security_group"
+  service_name = local.service_name
+  env    = terraform.workspace
+  vpc_id = data.aws_vpc.vpc.id
+  security_group_ingress_cidrs = [
+    "0.0.0.0/0" # 環境ごとに変えられるようにしておくとより良い
+  ]
+}
