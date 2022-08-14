db532dd4873935e101ee93063136df8e7703d14b
Ryoma Fujiwara <e238058r@hotmail.com>
2022-08-14 15:00:30 +0900

    ALB用セキュリティグループ設定の記述を修正

--- ecs/service/alb_security_group.tf
+++ ecs/service/alb_security_group.tf
@@ -4,11 +4,18 @@ module "alb_security_group" {
   service_name = local.service_name
   env    = terraform.workspace
   vpc_id = data.aws_vpc.vpc.id
+  #　内向き通信(ingress)設定
   security_group_ingress_cidrs = [
-    "0.0.0.0/0" # 環境ごとに変えられるようにしておくとより良い
+    "0.0.0.0/0"
   ]
+  # security_group_ingress_sgs = []
+  # security_group_ingress_allow_self = false
   security_group_ingress_ports = [
     80,
     443
   ]
+
+  # 外向け通信(egress)設定
+  # security_group_egress_cidrs = ["0.0.0.0/0"]
+  # security_group_egress_allow_self = false
 }
