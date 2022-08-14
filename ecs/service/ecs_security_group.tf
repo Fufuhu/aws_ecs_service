02951f4aab591cba3a7a3bcf5c905bf57d75a6d0
Ryoma Fujiwara <e238058r@hotmail.com>
2022-08-14 15:30:52 +0900

    ECSサービスで利用するセキュリティグループ設定を記述

---
+++ ecs/service/ecs_security_group.tf
@@ -0,0 +1,25 @@
+
+module "ecs_security_group" {
+  source = "../../modules/vpc/security_group"
+  service_name = local.service_name
+  service_suffix = "ecs-service"
+  env = terraform.workspace
+  vpc_id = data.aws_vpc.vpc.id
+
+  #　内向き通信(ingress)設定
+  security_group_ingress_cidrs = [
+    # "0.0.0.0/0"
+  ]
+  security_group_ingress_ports = [
+    80
+  ]
+  security_group_ingress_sgs = [
+    module.alb_security_group.security_group_id
+  ]
+
+
+  # 外向け通信(egress)設定
+  # security_group_egress_cidrs = ["0.0.0.0/0"]
+  # security_group_egress_allow_self = false
+  # security_group_egress_sgs = []
+}
