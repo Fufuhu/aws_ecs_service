64bb69e40e1e7308caadea6071ee701e34574d2f
Ryoma Fujiwara <e238058r@hotmail.com>
2022-08-14 15:22:41 +0900

    ECSサービスを配置するサブネットのIDリストを取得

--- ecs/service/subnets.tf
+++ ecs/service/subnets.tf
@@ -11,3 +11,17 @@ data "aws_subnets" "public_subnets" {
     Scope = "public"
   }
 }
+
+data "aws_subnets" "private_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [
+      data.aws_vpc.vpc.id
+    ]
+  }
+  tags = {
+    ServiceName = local.service_name
+    Env = terraform.workspace
+    Scope = "private"
+  }
+}
