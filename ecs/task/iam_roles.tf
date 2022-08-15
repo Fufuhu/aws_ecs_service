70d3668aa1d10b35c27a51e6a55828bba6f5af73
Ryoma Fujiwara <e238058r@hotmail.com>
2022-08-16 00:02:08 +0900

    default値をそのまま使うものをコメントアウトして提示

--- ecs/task/iam_roles.tf
+++ ecs/task/iam_roles.tf
@@ -2,4 +2,10 @@ module "roles" {
   source       = "../../modules/ecs/iam/roles"
   service_name = local.service_name
   env          = terraform.workspace
+
+  # task_exec_role_additional_tags = {}
+
+  # task_role_maneged_policy_arns = []
+  # task_role_inline_policies = {}
+  # task_exec_role_additional_tags = {}
 }
