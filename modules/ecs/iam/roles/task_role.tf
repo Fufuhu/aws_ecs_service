7be798eb7bd115cf61ffe3a21ebf8bceb50bbe60
Ryoma Fujiwara <e238058r@hotmail.com>
2022-08-16 00:01:36 +0900

    タスクロールのインラインポリシー記述を追加

--- modules/ecs/iam/roles/task_role.tf
+++ modules/ecs/iam/roles/task_role.tf
@@ -9,3 +9,10 @@ resource "aws_iam_role_policy_attachment" "task_role_policy_attachment" {
   role       = aws_iam_role.task_role.arn
   policy_arn = each.key
 }
+
+resource "aws_iam_role_policy" "task_role_inline_policies" {
+  for_each = var.task_role_inline_policies
+  name     = each.key
+  role     = aws_iam_role.task_role.arn
+  policy   = each.value
+}
